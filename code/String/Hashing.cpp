9a625e7c48cda2a57f7e2cbc9261c01b251122f8
Saif-Sakib <saifahmedsakib@gmail.com>
2026-09-08 12:21:23 +0600

    refactor(code): simplify hashing implementation and update notebook

--- code/String/Hashing.cpp
+++ code/String/Hashing.cpp
@@ -1,70 +1,44 @@
 const int N = 2e6 + 9;
-const int MOD1 = 127657753, MOD2 = 987654319;
-const int p1 = 137, p2 = 277;
-int ip1, ip2;
-pair<int, int> pw[N], ipw[N];
-void prec() {
-  pw[0] =  {1, 1};
-  for (int i = 1; i < N; i++) {
-    pw[i].first = 1LL * pw[i - 1].first * p1 % MOD1;
-    pw[i].second = 1LL * pw[i - 1].second * p2 % MOD2;
-  }
-  ip1 = power(p1, MOD1 - 2, MOD1);
-  ip2 = power(p2, MOD2 - 2, MOD2);
-  ipw[0] =  {1, 1};
-  for (int i = 1; i < N; i++) {
-    ipw[i].first = 1LL * ipw[i - 1].first * ip1 % MOD1;
-    ipw[i].second = 1LL * ipw[i - 1].second * ip2 % MOD2;
-  }
-}
-struct Hashing {
-  int n;
-  string s; // 0 - indexed
-  vector<pair<int, int>> hs; // 1 - indexed
-  Hashing() {}
-  Hashing(string _s) {
-    n = _s.size();
-    s = _s;
-    hs.emplace_back(0, 0);
-    for (int i = 0; i < n; i++) {
-      pair<int, int> p;
-      p.first = (hs[i].first + 1LL * pw[i].first * s[i] % MOD1) % MOD1;
-      p.second = (hs[i].second + 1LL * pw[i].second * s[i] % MOD2) % MOD2;
-      hs.push_back(p);
+const int M1 = 1e9 + 7, M2 = 1e9 + 9, B1 = 137, B2 = 277;
+pair<int, int> pw[N];
+
+void init_hash() {
+    pw[0] = {1, 1};
+    for (int i = 1; i < N; i++) {
+        pw[i].first = 1LL * pw[i - 1].first * B1 % M1;
+        pw[i].second = 1LL * pw[i - 1].second * B2 % M2;
     }
-  }
-  pair<int, int> get_hash(int l, int r) { // 1 - indexed
-    assert(1 <= l && l <= r && r <= n);
-    pair<int, int> ans;
-    ans.first = (hs[r].first - hs[l - 1].first + MOD1) * 1LL * ipw[l - 1].first % MOD1;
-    ans.second = (hs[r].second - hs[l - 1].second + MOD2) * 1LL * ipw[l - 1].second % MOD2;
-    return ans;
-  }
-  pair<int, int> get_hash() {
-    return get_hash(1, n);
-  }
+}
+struct Hash {
+    string s;
+    vector<pair<int, int>> hs;
+    Hash(const string& _s) : s(_s) {
+        hs.assign(s.size() + 1, {0, 0});
+        for (int i = 0; i < s.size(); i++) {
+            hs[i + 1].first = (1LL * hs[i].first * B1 + s[i]) % M1;
+            hs[i + 1].second = (1LL * hs[i].second * B2 + s[i]) % M2;
+        }
+    }
+    pair<int, int> get(int l, int r) { 
+        int h1 = (hs[r + 1].first - 1LL * hs[l].first * pw[r - l + 1].first % M1 + M1) % M1;
+        int h2 = (hs[r + 1].second - 1LL * hs[l].second * pw[r - l + 1].second % M2 + M2) % M2;
+        return {h1, h2};
+    }
+    int lcp(int i1, int j1, int i2, int j2) {
+        int l = 1, r = min(j1 - i1 + 1, j2 - i2 + 1), ans = 0;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (get(i1, i1 + mid - 1) == get(i2, i2 + mid - 1)) ans = mid, l = mid + 1;
+            else r = mid - 1;
+        }
+        return ans;
+    }
+    int compare(int i1, int j1, int i2, int j2) {
+        int l = lcp(i1, j1, i2, j2);
+        int len1 = j1 - i1 + 1, len2 = j2 - i2 + 1;
+        if (l == len1 && l == len2) return 0;
+        if (l == len1) return -1;
+        if (l == len2) return 1;
+        return s[i1 + l] < s[i2 + l] ? -1 : 1;
+    }
 };
-string s;
-Hashing hs;
-int lcp(int i1, int j1, int i2, int j2) {
-	int l = 1, r = min(j1 - i1 + 1, j2 - i2 + 1), ans = 0;
-	while (l <= r) {
-		int mid = (l + r) / 2;
-		if (hs.get_hash(i1 + 1, i1 + mid) == hs.get_hash(i2 + 1, i2 + mid)) {
-			ans = mid;
-			l = mid + 1;
-		}
-		else {
-			r = mid - 1;
-		}
-	}
-	return ans;
-}
-int compare(int i1, int j1, int i2, int j2) {
-	int l = lcp(i1, j1, i2, j2);
- 	int len1 = j1 - i1 + 1, len2 = j2 - i2 + 1;
-	if (len1 == len2 && l == len1) return 0;
- 	if (l == len1) return -1;
-	if (l == len2) return 1;
-	return s[i1 + l] < s[i2 + l] ? -1 : 1;
-}
